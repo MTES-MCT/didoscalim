50b2f572c3a574868dac135333ce6737b1438c31
Nicolas Chuche <nicolas.chuche@developpement-durable.gouv.fr>
2022-08-24 10:24:35 +0200

    test: add tests for my-organization

--- tests/testthat/test-my-organization.R
+++ tests/testthat/test-my-organization.R
@@ -19,3 +19,15 @@ test_that("my_organization errors if no organization found", {
 
   expect_error(my_organization("some random string"), "aucune organisation")
 })
+
+test_that("fails on multiple organizations", {
+  mockery::stub(my_organization, 'me', tibble(organizations = c("org1", "org2")))
+  expect_error(my_organization(), "Vous appartenez à plusieurs organisations")
+
+  mockery::stub(my_organization, 'me', tibble(organizations = c("org1", "org2")))
+  expect_error(my_organization("org"), "La recherche retourne plusieurs organisations")
+})
+
+test_that("fails when no organization is return", {
+  expect_error(my_organization("some weird org"), "La recherche ne retourne aucune organisation")
+})
