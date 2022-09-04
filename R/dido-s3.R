a51a78c9743a4c7725f5b85c2f8602901dbea748
Nicolas.Chuche <nicolas.chuche@developpement-durable.gouv.fr>
2022-09-04 11:30:16 +0200

    fix: work on check warning

--- R/dido-s3.R
+++ R/dido-s3.R
@@ -32,24 +32,24 @@ internal_clean_metadata <- function(data) UseMethod("internal_clean_metadata")
 #' ds <- list_datasets()[1, ]
 #' get_dataset_id(ds)
 #' @keywords internal
-get_dataset_id <- function(dataset) UseMethod("get_dataset_id")
+get_dataset_id <- function(data) UseMethod("get_dataset_id")
 
 #' @export
-get_dataset_id.default <- function(dataset) NULL
+get_dataset_id.default <- function(data) NULL
 
 #' @export
-get_dataset_id.character <- function(dataset) {
-  if (is_mongo_oid(dataset)) {
-    return(dataset)
+get_dataset_id.character <- function(data) {
+  if (is_mongo_oid(data)) {
+    return(data)
   }
   NULL
 }
 
 #' @export
-get_dataset_id.data.frame <- function(dataset) {
-  if (!"id" %in% names(dataset)) abort_not_dataset()
-  abort_if_not_one_line("dataset")
-  if (is_mongo_oid(dataset[["id"]])) return(dataset[["id"]])
+get_dataset_id.data.frame <- function(data) {
+  if (!"id" %in% names(data)) abort_not_dataset()
+  abort_if_not_one_line("data")
+  if (is_mongo_oid(data[["id"]])) return(data[["id"]])
   NULL
 }
 
@@ -64,24 +64,24 @@ get_dataset_id.data.frame <- function(dataset) {
 #' df <- list_datafiles()[1, ]
 #' get_datafile_rid(df)
 #' @keywords internal
-get_datafile_rid <- function(datafile) UseMethod("get_datafile_rid")
+get_datafile_rid <- function(data) UseMethod("get_datafile_rid")
 
 #' @export
-get_datafile_rid.default <- function(datafile) NULL
+get_datafile_rid.default <- function(data) NULL
 
 #' @export
-get_datafile_rid.character <- function(datafile) {
-  if (is_uuid(datafile)) {
-    return(datafile)
+get_datafile_rid.character <- function(data) {
+  if (is_uuid(data)) {
+    return(data)
   }
   NULL
 }
 
 #' @export
-get_datafile_rid.data.frame <- function(datafile) {
-  if (!"rid" %in% names(datafile)) abort_not_datafile()
-  abort_if_not_one_line("datafile")
-  if (is_uuid(datafile[["rid"]])) return(datafile[["rid"]])
+get_datafile_rid.data.frame <- function(data) {
+  if (!"rid" %in% names(data)) abort_not_datafile()
+  abort_if_not_one_line("data")
+  if (is_uuid(data[["rid"]])) return(data[["rid"]])
   NULL
 }
 
@@ -96,24 +96,24 @@ get_datafile_rid.data.frame <- function(datafile) {
 #' at <- list_attachments()[1, ]
 #' get_attachment_rid(at)
 #' @keywords internal
-get_attachment_rid <- function(attachment) UseMethod("get_attachment_rid")
+get_attachment_rid <- function(data) UseMethod("get_attachment_rid")
 
 #' @export
-get_attachment_rid.default <- function(attachment) NULL
+get_attachment_rid.default <- function(data) NULL
 
 #' @export
-get_attachment_rid.character <- function(attachment) {
-  if (is_uuid(attachment)) {
-    return(attachment)
+get_attachment_rid.character <- function(data) {
+  if (is_uuid(data)) {
+    return(data)
   }
   NULL
 }
 
 #' @export
-get_attachment_rid.data.frame <- function(attachment) {
-  if (!"rid" %in% names(attachment)) abort_not_attachment()
-  abort_if_not_one_line("attachment")
-  if (is_uuid(attachment[["rid"]])) return(attachment[["rid"]])
+get_attachment_rid.data.frame <- function(data) {
+  if (!"rid" %in% names(data)) abort_not_attachment()
+  abort_if_not_one_line("data")
+  if (is_uuid(data[["rid"]])) return(data[["rid"]])
   NULL
 }
 
@@ -140,8 +140,8 @@ get_job_id.character <- function(data) data
 get_job_id.integer <- function(data) data
 
 #' @export
-get_job_id.data.frame <- function(job_id) {
-  if (!"id" %in% names(job_id)) abort_not_job()
-  abort_if_not_one_line("job_id")
-  return(job_id[["id"]])
+get_job_id.data.frame <- function(data) {
+  if (!"id" %in% names(data)) abort_not_job()
+  abort_if_not_one_line("data")
+  return(data[["id"]])
 }
