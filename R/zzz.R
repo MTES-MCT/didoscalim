be1f6fa7a396045750f48c5ca08ace03efcc6d2d
Nicolas Chuche <nicolas.chuche@developpement-durable.gouv.fr>
2022-12-01 12:18:22 +0100

    fix: bug on install when Renviron isn't set

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,13 @@
 .onLoad <- function(libname, pkgname) { # nolint # nocov start
   load_envs()
-  set_work_env()
+  tryCatch(
+      set_work_env(),
+    error = function(error) {
+      local_didoscalim_verbosity("info")
+      didoscalim_info(c(
+        "x" = error$message
+      ))
+      return(FALSE)
+    }
+  )
 } # nocov end
